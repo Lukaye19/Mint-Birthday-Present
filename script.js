93dd08f9aea1e127e11fae20df3654043353c51c
Lukaye19 <roluka19@gmail.com>
2026-01-14 18:24:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function checkOrientation() {
+  if (window.innerHeight > window.innerWidth) {
+    document.body.classList.add("portrait");
+    document.body.classList.remove("landscape");
+  } else {
+    document.body.classList.add("landscape");
+    document.body.classList.remove("portrait");
+  }
+}
 
+window.addEventListener("resize", checkOrientation);
+checkOrientation();
